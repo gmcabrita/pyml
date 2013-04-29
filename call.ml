27538955322f685412c3adb883eab134cd8d9a73
Gonçalo Cabrita <_@gmcabrita.com>
2013-04-29 23:55:32 +0100

    Based OCaml, JK

--- call.ml
+++ call.ml
@@ -2,7 +2,7 @@ external call_python: string -> int -> int = "call_python"
 
 let pass_integer n = n;;
 
-let test_call = call_python "pass_integer" 10;;
+let test_call () = call_python "pass_integer" 10;;
 
 let () =
     Callback.register "pass_integer" pass_integer;
